8cea3a7f7fc479135b11b70dab2dc59903e4e999
jokerxin <jxpro@qq.com>
2024-12-28 16:38:00 +0800

    Handle connection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -7,5 +10,17 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection established!");
+
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(stream: TcpStream) {
+    let buf_header = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_header
+        .lines()
+        .map(|line| line.unwrap())
+        .take_while(|line| line.is_empty())
+        .collect();
+    println!("Request: {:#?}", http_request);
 }
