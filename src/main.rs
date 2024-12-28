371b2c1859e9519c2504c4392229641b4318d844
jokerxin <jxpro@qq.com>
2024-12-28 18:59:55 +0800

    Add 404 page

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,16 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_header = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_header
-        .lines()
-        .map(|line| line.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
 
-    let status = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
+    // 第一个 unwrap 是 Option::unwrap，第二个 unwrap 是 Result::unwrap
+    let request_line = buf_header.lines().next().unwrap().unwrap();
+
+    let (status, contents) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", fs::read_to_string("index.html").unwrap())
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", fs::read_to_string("404.html").unwrap())
+    };
+
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
         status,
